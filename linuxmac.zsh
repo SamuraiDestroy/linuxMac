f306b77f6885b9a7a5f7304bf88d7b8a2d89c8e2
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-11 21:06:31 +0100

    Hopefully added CPU detection.

--- linuxmac.zsh
+++ linuxmac.zsh
@@ -4,13 +4,25 @@ command -v brew >/dev/null 2>&1 || {
 }
 
 brew install curl sudo
+# Use brew to install necessary packages
 
-if [ "$1" == "fedora" ] || [ "$1" == "" ]; then
-    echo "Installing fedora..."
-    curl https://leifliddy.com/fedora.sh
-    git clone https://github.com/leifliddy/asahi-fedora-builder.git
-    cd asahi-fedora-builder
-    sudo zsh build.sh
-    cd ..
-    sudo zsh fedora.sh
+cpuHardware = sysctl -a | grep machdep.cpu.vendor
+
+if [[ $cpuHardware:l == *intel* ]]; then # This should return true if the CPU is Intel, needs testing though.
+    # Intel Distros
+    echo "This script currently does not support Intel CPUs, please wait for a future release."
+else
+    # Apple Silicon Distros
+    if [ "$1" == "fedora" ] || [ "$1" == "" ]; then
+        echo "Installing fedora..."
+        curl https://leifliddy.com/fedora.sh
+        git clone https://github.com/leifliddy/asahi-fedora-builder.git
+        cd asahi-fedora-builder
+        sudo zsh build.sh
+        cd ..
+        sudo zsh fedora.sh  
+    fi
 fi
+
+
+# sysctl -a | grep machdep.cpu.vendor should find out if the cpu is intel or apple (DO NOT GET RID OF THIS COMMENT UNTIL WE HAVE DETECTION WORKING)
