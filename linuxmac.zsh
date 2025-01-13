9d4dbae9b2720b0f9e4b494f7541db9bf94b195c
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-13 09:16:53 +0100

    Fixed stupid stuff

--- linuxmac.zsh
+++ linuxmac.zsh
@@ -4,18 +4,18 @@ command -v brew >/dev/null 2>&1 || {
 }
 # Check if brew is installed, if it isn't, install it.
 
-brew install curl sudo git
+brew install curl git
 # Use brew to install necessary packages
 
-cpuHardware = sysctl -a | grep machdep.cpu.vendor
+cpuHardware=$(sysctl -a | grep machdep.cpu.vendor)
 # Finds your CPU vendor
 
-if [[ $cpuHardware:l == *intel* ]]; then # This should return true if the CPU is Intel, needs testing though.
+case ("$cpuHardware:l" in
+    *"apple"*)
     # Intel Distros
-    echo "This script currently does not support Intel CPUs, please wait for a future release."
-else
+    echo "This script currently does not support Intel CPUs, please wait for a future release." ;;*)
     # Apple Silicon Distros
-    if [ "$1:l" == "fedora" ] || [ "$1" == "" ]; then # Check if the first parameter is either fedora or left blank
+    if [ "$1:l" = "fedora" ] || [ "$1" = "" ]; then # Check if the first parameter is either fedora or left blank
         echo "Installing fedora..."
         curl https://leifliddy.com/fedora.sh
         git clone https://github.com/leifliddy/asahi-fedora-builder.git
@@ -26,32 +26,32 @@ else
         sudo zsh fedora.sh
         # Executes the files
         # Below this is the switch statement for all distros that can be run on Apple Silicon CPUs, even though they are unimplemented.
-    elif [["$1:l" == "arch"]] then
+    elif [ "$1:l" = "arch" ]; then
         echo "Arch is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "aosc"]] then
+    elif [ "$1:l" = "aosc" ]; then
         echo "AOSC is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "centos"]] then
+    elif [ "$1:l" = "centos" ]; then
         echo "CentOS is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "debian"]] then
+    elif [ "$1:l" = "debian" ]; then
         echo "Debian is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "deepin"]] then
+    elif [ "$1:l" = "deepin" ]; then
         echo "Deepin is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "fedora"]] then
+    elif [ "$1:l" = "fedora" ]; then
         echo "Fedora is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "gentoo"]] then
+    elif [ "$1:l" = "gentoo" ]; then
         echo "Gentoo is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "nixos"]] then
+    elif [ "$1:l" = "nixos" ]; then
         echo "NoxOS is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "rocky linux"]] then
+    elif [ "$1:l" = "rocky linux" ]; then
         echo "Rocky Linux is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "ubuntu"]] then
-        echo "Ubuntu is currently not implemented, wait for a future release." 
-    elif [["$1:l" == "void linux"]] then
+    elif [ "$1:l" = "ubuntu" ]; then
+        echo "Ubuntu is currently not implemented, wait for a future release. Also, fuck you." 
+    elif [ "$1:l" = "void linux" ]; then
         echo "Void Linux is currently not implemented, wait for a future release." 
     else
-        echo "The distro you have selected currently does not support Apple Silicon and there are currently no plans to implement it." 
-    fi
-fi
+        echo "The distro you have selected currently does not support Apple Silicon (or you have misspelled the name) and there are currently no plans to implement it." 
+    fi ;;
+esac
 
 
 # sysctl -a | grep machdep.cpu.vendor should find out if the cpu is intel or apple (DO NOT GET RID OF THIS COMMENT UNTIL WE HAVE DETECTION WORKING)
