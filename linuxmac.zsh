0011204a4348c3dc376f1c6ca2223394f649fcb9
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-18 11:58:53 +0100

    Switched from using parameters to user input (if it works)

--- linuxmac.zsh
+++ linuxmac.zsh
@@ -19,44 +19,46 @@ case "$cpuHardware:l" in
     # Intel Distros
     echo "This script currently does not support Intel CPUs, please wait for a future release." ;;*)
     # Apple Silicon Distros
-    if [ "$1:l" = "fedora" ] || [ "$1" = "" ]; then # Check if the first parameter is either fedora or left blank
+    echo "Please type in the name of the distro from the list you want to install and then press enter [fedora, alpinelinux, arch, aosc, centos, debian, deepin, gentoo, nixos, rockylinux, ubuntu, voidlinux]: "
+    read distro
+    if [ "$distro:l" = "fedora" ]; then # Check if the first parameter is either fedora or left blank
 	echo "Installing fedora..."
     curl https://alx.sh | sh
 	# Below this is the switch statement for all distros that can be run on Apple Silicon CPUs, even though they are unimplemented.
-    elif [ "$1:l" = "alpinelinux" ]; then
+    elif [ "$distro:l" = "alpinelinux" ]; then
         #echo "Alpine Linux is currently not implemented, wait for a future release"
         curl https://arvanta.net/asahi/aai.sh | sh
-    elif [ "$1:l" = "arch" ]; then
+    elif [ "$distro:l" = "arch" ]; then
         #echo "Arch is currently not implemented, wait for a future release." 
         echo "This one requires stupid config stuff go to https://github.com/asahi-alarm/asahi-alarm/blob/main/manual-install.md and read about it after the installer is done. (Just click the link and it should take you right there.)"
         curl -SL https://github.com/asahi-alarm/asahi-alarm/releases/download/installer/installer-bootstrap.sh | sh
-    elif [ "$1:l" = "aosc" ]; then
+    elif [ "$distro:l" = "aosc" ]; then
         #echo "AOSC is currently not implemented, wait for a future release." 
         curl -sSf https://repo.aosc.io/get-oma.sh | sudo sh
-    elif [ "$1:l" = "centos" ]; then
+    elif [ "$distro:l" = "centos" ]; then
         #echo "CentOS is currently not implemented, wait for a future release." 
         echo "Root password for this is centosstream"
         curl https://ecurtin.fedorapeople.org/centos.sh | sh
-    elif [ "$1:l" = "debian" ]; then
+    elif [ "$distro:l" = "debian" ]; then
         # The Debian installer needs to be tested outside of this script because the documentation for it might just be wrong.
         echo "Debian is currently not implemented, wait for a future release." 
-    elif [ "$1:l" = "deepin" ]; then
+    elif [ "$distro:l" = "deepin" ]; then
         # More research is necessary for this one since it's in a different language
         echo "Deepin is currently not implemented, wait for a future release."  
-    elif [ "$1:l" = "gentoo" ]; then
+    elif [ "$distro:l" = "gentoo" ]; then
         # After reading the guide, AAAAAAAAAAAAAA
         echo "Gentoo is currently not implemented, wait for a future release." 
-    elif [ "$1:l" = "nixos" ]; then
+    elif [ "$distro:l" = "nixos" ]; then
         # Later.
         echo "NixOS is currently not implemented, wait for a future release." 
-    elif [ "$1:l" = "rockylinux" ]; then
+    elif [ "$distro:l" = "rockylinux" ]; then
         #echo "Rocky Linux is currently not implemented, wait for a future release." 
         echo "Root password for this is rocky"
         curl https://leifliddy.com/rocky.sh | sh
-    elif [ "$1:l" = "ubuntu" ]; then
+    elif [ "$distro:l" = "ubuntu" ]; then
         #echo "Ubuntu is currently not implemented, wait for a future release." 
         curl -sL https://ubuntuasahi.org/install | sh
-    elif [ "$1:l" = "voidlinux" ]; then
+    elif [ "$distro:l" = "voidlinux" ]; then
         #echo "Void Linux is currently not implemented, wait for a future release."
         echo "Root password for this is voidlinux"
         curl -L https://tinyurl.com/void-asahi | sh 
