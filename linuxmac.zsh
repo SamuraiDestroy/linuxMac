4e144e1cb1b09cfbd3581ab7e390ac78ff8ede8a
SamuraiDestroy <01PMvictor@gmail.com>
2025-02-01 10:08:59 +0100

    feat: Some amount of parity and also fixed cases again

--- linuxmac.zsh
+++ linuxmac.zsh
@@ -1,6 +1,7 @@
 case "$USER:l" in
     *"djafar"*)
-    echo "Hi Djafar" ;;*)
+    echo "Hi Djafar" 
+    sleep 0.5 ;;*)
 esac
 # Hi Djafar.
 
@@ -11,17 +12,21 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/in
 brew install curl git
 # Use brew to install necessary packages
 
-cpuHardware=$(sysctl -n machdep.cpu.brand_string)
+cpuHardware=$(sysctl -n machdep.cpu.brand_string | tr '[:upper:]' '[:lower:]')
 # Finds your CPU vendor
 
-case ${cpuHardware,,} in
+echo "In the unlikely case of corruption and/or data loss, the developers of this script are not responsible.\nIt is recommended to make backups before running this script."
+sleep 2
+
+case $cpuHardware in
     *"intel"*)
     # Intel Distros
     echo "This script currently does not support Intel CPUs, please wait for a future release." ;;*)
     # Apple Silicon Distros
      echo "Please type in the name of the distro from the list you want to install and then press enter [fedora, alpinelinux, arch, aosc, centos, debian, deepin, gentoo, nixos, rockylinux, ubuntu, voidlinux]: "
     read distro # Get user input of what distro they want to use
-    case ${distro,,} in 
+    distro=$(echo "$distro" | tr '[:upper:]' '[:lower:]')
+    case $distro in 
     "fedora") 
     # Check if the first parameter is fedora
 	    echo "Installing fedora..."
@@ -31,6 +36,7 @@ case ${cpuHardware,,} in
     "alpinelinux"*)
         #echo "Alpine Linux is currently not implemented, wait for a future release"
         echo "The default user for this is root"
+        sleep 1.5
         curl https://arvanta.net/asahi/aai.sh | sh ;;
     *)
         echo "The distro you have selected currently does not support Apple Silicon (or you misspelled it) and there are currently no plans to implement it." ;;
