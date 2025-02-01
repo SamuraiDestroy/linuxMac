e0256a9fe2ceaefcafad4155b5cbe0744d96d07d
SamuraiDestroy <01PMvictor@gmail.com>
2025-02-01 09:53:44 +0100

    feat: Added sleeps to allow people to read important stuff

--- linuxmac.zsh
+++ linuxmac.zsh
@@ -1,6 +1,8 @@
 case "$USER:l" in
     *"djafar"*)
-    echo "Hi Djafar" ;;*)
+    echo "Hi Djafar"
+    sleep 0.5
+    ;;*)
 esac
 # Hi Djafar.
 
@@ -21,6 +23,7 @@ cpuHardware=$(sysctl -n machdep.cpu.brand_string | tr '[:upper:]' '[:lower:]')
 # Finds your CPU vendor
 
 echo "In the unlikely case of corruption and/or data loss, the developers of this script are not responsible.\nIt is recommended to make backups before running this script."
+sleep 2
 
 case $cpuHardware in
     *"intel"*)
@@ -51,10 +54,12 @@ case $cpuHardware in
 	# More distro checking below this haha
     "alpinelinux"*)
         echo "The default user for this is root"
+        sleep 1.5
         curl https://arvanta.net/asahi/aai.sh | sh ;;
     "arch"*)
         #echo "Arch is currently not implemented, wait for a future release." 
         echo "This one requires stupid config stuff go to https://github.com/asahi-alarm/asahi-alarm/blob/main/manual-install.md and read about it after the installer is done. (Just click the link and it should take you right there.)"
+        sleep 2
         curl -SL https://github.com/asahi-alarm/asahi-alarm/releases/download/installer/installer-bootstrap.sh | sh ;;
     "aosc"*)
         #echo "AOSC is currently not implemented, wait for a future release." 
@@ -62,6 +67,7 @@ case $cpuHardware in
     "centos"*)
         #echo "CentOS is currently not implemented, wait for a future release." 
         echo "Root password for this is centosstream"
+        sleep 1.5
         curl https://ecurtin.fedorapeople.org/centos.sh | sh ;;
     "debian"*)
         # The Debian installer needs to be tested outside of this script because the documentation for it might just be wrong.
@@ -75,13 +81,14 @@ case $cpuHardware in
         echo "Gentoo is currently not implemented, wait for a future release." 
         if [ $devmode ]; then
             echo "You need a USB stick plugged in for this one"
-
+            sleep 1.5
         fi ;;
     "nixos"*)
         # Later.
         echo "NixOS is currently not implemented, wait for a future release." 
         if [ $devmode ]; then
             echo "You need a USB stick plugged in for this one"
+            sleep 1.5
             git clone https://github.com/tpwrules/nixos-apple-silicon/
             cd nixos-apple-silicon
             nix build --extra-experimental-features 'nix-command flakes' .#m1n1 -o m1n1
@@ -93,6 +100,7 @@ case $cpuHardware in
     "rockylinux"*)
         #echo "Rocky Linux is currently not implemented, wait for a future release." 
         echo "Root password for this is rocky" 
+        sleep 1.5
         curl https://leifliddy.com/rocky.sh | sh ;;
     "ubuntu"*)
         #echo "Ubuntu is currently not implemented, wait for a future release." 
@@ -100,6 +108,7 @@ case $cpuHardware in
     "voidlinux"*)
         #echo "Void Linux is currently not implemented, wait for a future release."
         echo "Root password for this is voidlinux"
+        sleep 1.5
         curl -L https://tinyurl.com/void-asahi | sh ;;
     *)
         echo "The distro you have selected currently does not support Apple Silicon (or you misspelled it) and there are currently no plans to implement it." ;;
